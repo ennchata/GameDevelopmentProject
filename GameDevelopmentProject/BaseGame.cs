6ea5b4b1e9c3fde24814b877be4c0e4c303715af
tmaes <140080232+ennchata@users.noreply.github.com>
2024-11-08 14:18:45 +0100

    Cleanup Game instance file

--- GameDevelopmentProject/BaseGame.cs
+++ GameDevelopmentProject/BaseGame.cs
@@ -3,41 +3,36 @@ using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 
 namespace GameDevelopmentProject {
-    public class Game1 : Game {
-        private GraphicsDeviceManager _graphics;
-        private SpriteBatch _spriteBatch;
+    public class BaseGame : Game {
+        private GraphicsDeviceManager graphics;
+        private SpriteBatch spriteBatch;
 
-        public Game1() {
-            _graphics = new GraphicsDeviceManager(this);
+        public BaseGame() {
+            graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
             IsMouseVisible = true;
         }
 
         protected override void Initialize() {
-            // TODO: Add your initialization logic here
+            graphics.PreferredBackBufferWidth = 1280;
+            graphics.PreferredBackBufferHeight = 720;
+            graphics.ApplyChanges();
 
             base.Initialize();
         }
 
         protected override void LoadContent() {
-            _spriteBatch = new SpriteBatch(GraphicsDevice);
-
-            // TODO: use this.Content to load your game content here
+            spriteBatch = new SpriteBatch(GraphicsDevice);
         }
 
         protected override void Update(GameTime gameTime) {
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
-
-            // TODO: Add your update logic here
-
             base.Update(gameTime);
         }
 
         protected override void Draw(GameTime gameTime) {
             GraphicsDevice.Clear(Color.Black);
-
-            // TODO: Add your drawing code here
 
             base.Draw(gameTime);
         }
--- GameDevelopmentProject/Program.cs
+++ GameDevelopmentProject/Program.cs
@@ -1,3 +1,3 @@
 ﻿
-using var game = new GameDevelopmentProject.Game1();
+using var game = new GameDevelopmentProject.BaseGame();
 game.Run();
