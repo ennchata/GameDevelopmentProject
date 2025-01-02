1056244b3c4a72d012cb382e83ed6745051d7760
tmaes <140080232+ennchata@users.noreply.github.com>
2025-01-02 16:44:53 +0100

    Adjustments to pause

--- GameDevelopmentProject/App/Pause/ControlScreen.cs
+++ GameDevelopmentProject/App/Pause/ControlScreen.cs
@@ -1,6 +1,7 @@
 ﻿using GameDevelopmentProject.Components.Drawables;
 using GameDevelopmentProject.Components.Screens;
 using GameDevelopmentProject.Components.UI;
+using GameDevelopmentProject.Components.UI.Commands;
 using Microsoft.Xna.Framework;
 using System;
 using System.Collections.Generic;
@@ -19,6 +20,30 @@ namespace GameDevelopmentProject.App.Pause {
                 GlobalAnchor = Anchor.TOP_CENTER,
                 LocalAnchor = Anchor.TOP_CENTER,
                 Position = new Vector2(0, 10)
+            });
+
+            Add(new TextDrawable(game) {
+                Text = "Press Esc to continue game",
+                AssetReference = "Fonts/Default",
+                Color = Color.Yellow,
+                GlobalAnchor = Anchor.TOP_CENTER,
+                LocalAnchor = Anchor.TOP_CENTER,
+                Position = new Vector2(0, 50)
+            });
+
+            Add(new Button(game) {
+                ButtonSize = new Vector2(300, 60),
+                Position = new Vector2(0, 100),
+                Text = "Quit Game",
+                AssetReference = "Fonts/Default",
+                Color = Color.White,
+                BackgroundColor = Color.Red,
+                HoverColor = Color.Lerp(Color.Red, Color.Black, 0.15f),
+                GlobalAnchor = Anchor.TOP_CENTER,
+                LocalAnchor = Anchor.TOP_CENTER,
+                Commands = new IButtonCommand[] {
+                    new GameExitCommand(game)
+                }
             });
         }
     }
